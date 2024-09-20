c5d291ed8ef58016aba95075f7516ca3f6521b45
Aryan Sharma <78246636+AryanSharma02@users.noreply.github.com>
2024-09-21 00:00:50 +0530

    added the Browse card part

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
 import './App.css';
+import BrowseHouses from './Components/Browse Houses/BrowseHouses';
 
 function App() {
   return (
     <div className="App">
-          <div class="hero-image-container"></div>
-    <div class="hero-image-container-filter"></div>
-    <div class="supreme-container">
+          <div className="hero-image-container"></div>
+    <div className="hero-image-container-filter"></div>
+    <div className="supreme-container">
         <header>
-            <div class="navbar">
-                <div class="logo"><h2>realtor.com</h2></div>    
-                <ul class="navigation-list">
-                    <li class="navigation-list-item">Buy</li>
-                    <li class="navigation-list-item">Sell</li>
-                    <li class="navigation-list-item">Rent</li>
-                    <li class="navigation-list-item">Mortgage</li>
-                    <li class="navigation-list-item">Find realtors</li>
-                    <li class="navigation-list-item">My Home</li>
-                    <li class="navigation-list-item">News & Insights</li>   
+            <div className="navbar">
+                <div className="logo"><h2>realtor.com</h2></div>    
+                <ul className="navigation-list">
+                    <li className="navigation-list-item">Buy</li>
+                    <li className="navigation-list-item">Sell</li>
+                    <li className="navigation-list-item">Rent</li>
+                    <li className="navigation-list-item">Mortgage</li>
+                    <li className="navigation-list-item">Find realtors</li>
+                    <li className="navigation-list-item">My Home</li>
+                    <li className="navigation-list-item">News & Insights</li>   
                 </ul>
-                <div class="navbar-login-signup-btn">
+                <div className="navbar-login-signup-btn">
                     <button>Log in</button>
                     <button>Sign up</button>
                 </div>
@@ -27,27 +28,38 @@ function App() {
         
         <main>
             
-            <div class="hero-container-primary">
-                <div class="hero-container-secondary">
-                <div class="hero-heading">
+            <div className="hero-container-primary">
+                <div className="hero-container-secondary">
+                <div className="hero-heading">
                     <h1>The #1 site real estate</h1>
                     <h1>professionals trust*</h1>
                 </div>
-                <ul class="hero-navigation-list">
-                    <li class="navigation-list-item">Buy</li>
-                    <li class="navigation-list-item">Sell</li>
-                    <li class="navigation-list-item">Rent</li>
-                    <li class="navigation-list-item">Pre-approval</li>
-                    <li class="navigation-list-item">Just sold</li>
-                    <li class="navigation-list-item">Home value</li>    
+                <ul className="hero-navigation-list">
+                    <li className="navigation-list-item">Buy</li>
+                    <li className="navigation-list-item">Sell</li>
+                    <li className="navigation-list-item">Rent</li>
+                    <li className="navigation-list-item">Pre-approval</li>
+                    <li className="navigation-list-item">Just sold</li>
+                    <li className="navigation-list-item">Home value</li>    
                 </ul>
-                <div class="hero-search">
+                <div className="hero-search">
                     <input type="text" placeholder="Address, School, City, Zip or Neighborhood"/>
                 </div>
             </div>
             </div>
-            <div class="browse-homes">Browse</div>
-            <div class="discover-help">Discover</div>   
+            <div className="browse-homes-heading">Browse homes in Vero Beach, FL</div>
+            <div className="browse-homes"> 
+                <BrowseHouses/>
+                <BrowseHouses/>
+                <BrowseHouses/>
+                <BrowseHouses/> 
+                <BrowseHouses/>
+                <BrowseHouses/>
+                <BrowseHouses/>
+                <BrowseHouses/>
+
+            </div>
+            <div className="discover-help">Discover</div>   
         </main>
         <footer></footer>
     </div>  

83f57996f33ee1b40beac524c7e167a66f3ff9d5
vibhucodes-debug <vaibhavsharma2124@gmail.com>
2024-09-21 08:17:02 +0530

    Merged newly created compnents Browse and HeroContainer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,26 +16,7 @@ function App() {
         </header>
         
         <main>
-            
-            <div className="hero-container-primary">
-                <div className="hero-container-secondary">
-                <div className="hero-heading">
-                    <h1>The #1 site real estate</h1>
-                    <h1>professionals trust*</h1>
-                </div>
-                <ul className="hero-navigation-list">
-                    <li className="navigation-list-item">Buy</li>
-                    <li className="navigation-list-item">Sell</li>
-                    <li className="navigation-list-item">Rent</li>
-                    <li className="navigation-list-item">Pre-approval</li>
-                    <li className="navigation-list-item">Just sold</li>
-                    <li className="navigation-list-item">Home value</li>    
-                </ul>
-                <div className="hero-search">
-                    <input type="text" placeholder="Address, School, City, Zip or Neighborhood"/>
-                </div>
-            </div>
-            </div>
+            <HeroContainer/>
             <div className="browse-homes-heading">Browse homes in Vero Beach, FL</div>
             <div className="browse-homes"> 
                 <BrowseHouses/>
@@ -46,13 +27,10 @@ function App() {
                 <BrowseHouses/>
                 <BrowseHouses/>
                 <BrowseHouses/>
-
             </div>
             <div className="discover-help">Discover</div>   
-            <HeroContainer/>
-            <div class="browse-homes">Browse</div>
-            <div class="discover-help">Discover</div>   
         </main>
+        
         <footer></footer>
     </div>  
     </div>

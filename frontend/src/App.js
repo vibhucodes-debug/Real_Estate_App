87ccfaf28db851907d128167c834c0206c7d3dae
vibhucodes-debug <vaibhavsharma2124@gmail.com>
2024-09-22 10:31:31 +0530

    Moved Read Article out of supreme continer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,40 +7,41 @@ import ReadArticle from './Components/readArticle/readArticle.jsx';
 import StyledArticles from './Components/styledArticles/styledArticles.jsx';
 
 function App() {
-  return (
-    <div className="App">
-        
-        <div className="hero-image-container"></div>
-        <div className="hero-image-container-filter"></div>
-        <div className="supreme-container">
+    return (
+        <div className="App">
 
-        <header>
-           <Navbar/>
-        </header>
-        
-        <main>
-            <HeroContainer/>
-            <div className="browse-homes-heading">Browse homes in Vero Beach, FL</div>
-            <div className="browse-homes"> 
-                <BrowseHouses/>
-                <BrowseHouses/>
-                <BrowseHouses/>
-                <BrowseHouses/> 
-                <BrowseHouses/>
-                <BrowseHouses/>
-                <BrowseHouses/>
-                <BrowseHouses/>
+            <div className="hero-image-container"></div>
+            <div className="hero-image-container-filter"></div>
+            <div className="supreme-container">
+
+                <header>
+                    <Navbar />
+                </header>
+
+                <main>
+                    <HeroContainer />
+                    <div className="browse-homes-heading">Browse homes in Vero Beach, FL</div>
+                    <div className="browse-homes">
+                        <BrowseHouses />
+                        <BrowseHouses />
+                        <BrowseHouses />
+                        <BrowseHouses />
+                        <BrowseHouses />
+                        <BrowseHouses />
+                        <BrowseHouses />
+                        <BrowseHouses />
+                    </div>
+                    <DiscoverHelp />
+
+
+                </main>
+
+                <footer></footer>
             </div>
-            <DiscoverHelp/> 
-            <ReadArticle/>
-            <StyledArticles/>
-
-        </main>
-
-        <footer></footer>
-    </div>  
-    </div>
-  );
+            <ReadArticle />
+            <StyledArticles />
+        </div>
+    );
 }
 
 export default App;
--- frontend/src/Components/styledArticles/styledArticles.jsx
+++ frontend/src/Components/styledArticles/styledArticles.jsx
@@ -9,27 +9,27 @@ const StyledArticles = () => {
 
     return (
         <div className="styled-article-container">
-            <div className="styled-article-card">
-                <div className="styled-article-image" style={{backgroundImage:`URL(${styledArticleImage1})`}}></div>
-                <div className="styled-article-text">
-                    Mortgage Rates Dive to 6.09% as Housing Market Shows 'Sense of...
+                <div className="styled-article-card">
+                    <div className="styled-article-image" style={{backgroundImage:`URL(${styledArticleImage1})`}}></div>
+                    <div className="styled-article-text">
+                        Mortgage Rates Dive to 6.09% as Housing Market Shows 'Sense of...
+                    </div>
                 </div>
-            </div>
 
-            <div className="styled-article-card">
-                <div className="styled-article-image" style={{backgroundImage:`url(${styledArticleImage2})`}}></div>
-                <div className="styled-article-text">Boomers Own 38% of America's Homes-but More than Half...</div>
-            </div>
+                <div className="styled-article-card">
+                    <div className="styled-article-image" style={{backgroundImage:`url(${styledArticleImage2})`}}></div>
+                    <div className="styled-article-text">Boomers Own 38% of America's Homes-but More than Half...</div>
+                </div>
 
-            <div className="styled-article-card">
-                <div className="styled-article-image" style={{backgroundImage:`url(${styledArticleImage3})`}}></div>
-                <div className="styled-article-text">The Magic Mortgage Rate That Cloud Psychologically Jump-Sta...</div>
-            </div>
+                <div className="styled-article-card">
+                    <div className="styled-article-image" style={{backgroundImage:`url(${styledArticleImage3})`}}></div>
+                    <div className="styled-article-text">The Magic Mortgage Rate That Cloud Psychologically Jump-Sta...</div>
+                </div>
 
-            <div className="styled-article-card">
-                <div className="styled-article-image" style={{backgroundImage:`url(${styledArticleImage4})`}}></div>
-                <div className="styled-article-text">Mira Sorvino's Design Request Gets Flat-Out Denied On...</div>
-            </div>
+                <div className="styled-article-card">
+                    <div className="styled-article-image" style={{backgroundImage:`url(${styledArticleImage4})`}}></div>
+                    <div className="styled-article-text">Mira Sorvino's Design Request Gets Flat-Out Denied On...</div>
+                </div>
 
         </div>
     )
